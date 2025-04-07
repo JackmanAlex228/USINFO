3557cf8d054d3264a869814cb2e1b27ca4a53d59
JackmanAlex228 <jackman.alex.w@gmail.com>
2025-04-07 15:12:38 -0600

    added swiftui code with proper directory structure

---
+++ ios-frontend/USINFO/USINFO/USINFOApp.swift
@@ -0,0 +1,17 @@
+//
+//  USINFOApp.swift
+//  USINFO
+//
+//  Created by Alex Jackman on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct USINFOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-frontend/USINFO/USINFOTests/USINFOTests.swift
@@ -0,0 +1,17 @@
+//
+//  USINFOTests.swift
+//  USINFOTests
+//
+//  Created by Alex Jackman on 4/7/25.
+//
+
+import Testing
+@testable import USINFO
+
+struct USINFOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
